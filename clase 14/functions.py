d0d592591205c3b1356622428755a06f13abda1b
GuillermoCochrane <guilleac81@gmail.com>
2024-07-26 21:26:33 -0300

    Archivos clase 14

---
+++ clase 14/functions.py
@@ -0,0 +1,7 @@
+def impresora(texto:str):
+    print(texto)
+    
+impresora("¿No lo ha visto a Don Soborno, guiño guiño?")
+
+for i in range(10):
+    impresora(f"Esta es vuelta número {i}")
---
+++ clase 14/p.md
@@ -0,0 +1,2 @@
+ctrl + d =  seleccion multiple (a medida que se presiona, selecciona los siguiente iguales a la selección actual)
+ctrl + h = reemplazar 
