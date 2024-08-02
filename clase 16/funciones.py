f96c1534a3a7b374f97c7648b5f7093b54eb41ab
GuillermoCochrane <guilleac81@gmail.com>
2024-08-01 22:34:50 -0300

    importamos y utilizamos funciones de otro archivo, documentamos funciones antiguas

--- clase 16/funciones.py
+++ clase 16/funciones.py
@@ -1,12 +1,28 @@
 # Nuestras funciones
 
+import mis_funciones as mf
+
 # saludar (procedimiento)
 def saludar(nombre:str | None = ",te olvidaste el parámetro "):
+    """
+    Imprime un saludo con el nombre que se le ingresa como parámetro.
+    Si no se le ingresa el parámetro, se imprime "Hola, te olvidaste el parámetro"
+    """
     print("Hola " + nombre)
 
 # calcular area rectangulo (función)
 def area_rectangulo(largo:int | None = 0, alto: int | None = 0) -> int:
+    """
+    Calcula la área de un rectangulo con los parámetros que se le ingresan.
+    Si no se le ingresan los parámetros, devuelve 0 (0 x 0)
+    """
+    if largo == 0 or alto == 0:
+        largo = 1
+        alto = 1
     return largo * alto
 
-saludar()
-print(area_rectangulo())
+numero1 = mf.validador_de_numero()
+numero2 = mf.validador_de_numero()
+print(mf.sumar(numero1, numero2))
+# saludar()
+# print(area_rectangulo())
