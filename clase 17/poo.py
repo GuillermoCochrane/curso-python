39b6738ecdff3858d34aae0230bba3412035cfca
GuillermoCochrane <guilleac81@gmail.com>
2024-08-15 20:00:22 -0300

    Ejercicio hecho en la clase de POO

---
+++ clase 17/poo.py
@@ -0,0 +1,28 @@
+#definiendo objetos
+
+class vehiculos: 
+    def __init__(self, color, marca , modelo, puertas):
+        self.color = color
+        self.marca = marca
+        self.modelo = modelo
+        self.puertas = puertas
+        
+    def mostrar_caracteristicas(self):
+        print(f"El color de este {self.marca} {self.modelo} es {self.color} y tiene {self.puertas} puertas")
+        
+    def datos_vehiculo(self):
+        return
+
+class autos: 
+    def __init__(self):
+        self.color = "azul"
+        self.marca = "Ford"
+        self.modelo = "Fiesta"
+        self.puertas = 4
+        
+    def mostrar_caracteristicas(self):
+        print(f"El color de este {self.marca} {self.modelo} es {self.color} y tiene {self.puertas} puertas")
+
+auto_diego = vehiculos("rojo", "Ford", "Fiesta", 4)
+auto_diego.mostrar_caracteristicas()
+print(f"el color del auto es {auto_diego.color}")
