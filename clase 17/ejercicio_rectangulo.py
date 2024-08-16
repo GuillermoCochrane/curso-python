d6763fa97dff4ce96bf4a9075d5e26d8ab84090a
GuillermoCochrane <guilleac81@gmail.com>
2024-08-15 21:27:03 -0300

    Ejercicio 1 de la teoría

---
+++ clase 17/ejercicio_rectangulo.py
@@ -0,0 +1,14 @@
+class Rectangulo:
+    def __init__(self, ancho, alto):
+        self.ancho = ancho
+        self.alto = alto
+
+    def area(self):
+        return self.ancho * self.alto
+
+    def perimetro(self):
+        return 2 * (self.ancho + self.alto)
+
+rect1 = Rectangulo(10, 20)
+print(rect1.area())
+print(rect1.perimetro())
