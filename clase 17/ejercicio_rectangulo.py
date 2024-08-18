b5a7eb7dcf4fcce1589447595abb566a37079303
GuillermoCochrane <guilleac81@gmail.com>
2024-08-17 22:28:01 -0300

    Ejercicio 1 POO de teoría

--- clase 17/ejercicio_rectangulo.py
+++ clase 17/ejercicio_rectangulo.py
@@ -8,7 +8,9 @@ class Rectangulo:
 
     def perimetro(self):
         return 2 * (self.ancho + self.alto)
+    
+    def caracteristicas(self):
+        print(f"El rectangulo tiene un ancho de {self.ancho} y un alto de {self.alto}, con un area de {self.area()} y un perímetro de {self.perimetro()}")
 
 rect1 = Rectangulo(10, 20)
-print(rect1.area())
-print(rect1.perimetro())
+rect1.caracteristicas()
