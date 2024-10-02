73a613ac7830a9c14ddde6499f624f8156bfc5bd
GuillermoCochrane <guilleac81@gmail.com>
2024-10-01 22:38:28 -0300

    Primera versión del lab 3: app del tiempo

---
+++ labs3/main.py
@@ -0,0 +1,18 @@
+import requests
+
+def obtener_clima():
+    ciudad = "General Levalle" #entrada.get()
+    api_key = "bc899cb7e122434a93910705240210"
+    try:
+        url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={ciudad}&lang=es"
+        respuesta = requests.get(url)
+        clima = respuesta.json()
+        #print(clima)
+        if "errror" in clima:
+          label_clima["text"] = f"error: {clima['error']["message"]}"
+        else:
+          label_clima["text"] = f" El clima es {clima['current']['condition']['text']} y la temperatura es {clima['current']['temp_c']}°C"
+    except:
+        label_clima["text"] = "error: no se pudo obtener la información"
+
+obtener_clima()
